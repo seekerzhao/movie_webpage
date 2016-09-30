861be77afbdb8b088d1fda405eb29c8f64465f9f
seekerzhao <seeker8@163.com>
2016-09-30 13:42:08 +0800

    changeMalong image url

--- entertainment.py
+++ entertainment.py
@@ -6,20 +6,20 @@ funnyVedio = media.Movie("funnyVedio", "a funny vedio",
 			"https://www.youtube.com/watch?v=Xs0AupRWyC0")
 
 MaGirl_1 = media.Movie("MaLong", "Ma long's beautiful girlfriend",
-					"http://img5.imgtn.bdimg.com/it/u=900789529,877641406&fm=11&gp=0.jpg",
+					"http://img1.imgtn.bdimg.com/it/u=4002825267,2389293098&fm=11&gp=0.jpg",
 					"https://www.youtube.com/watch?v=Xs0AupRWyC0")
 
 MaGirl_2 = media.Movie("MaLong", "Ma long's beautiful girlfriend",
-					"http://img5.imgtn.bdimg.com/it/u=900789529,877641406&fm=11&gp=0.jpg",
+					"http://wh.yesky.com/uploadImages/upload/20160924/sbe5cpfwludjpg.jpg",
 					"https://www.youtube.com/watch?v=Xs0AupRWyC0")
 MaGirl_3 = media.Movie("MaLong", "Ma long's beautiful girlfriend",
-					"http://img5.imgtn.bdimg.com/it/u=900789529,877641406&fm=11&gp=0.jpg",
+					"http://upload.jxntv.cn/2016/0812/1470965214544.jpg",
 					"https://www.youtube.com/watch?v=Xs0AupRWyC0")
 MaGirl_4 = media.Movie("MaLong", "Ma long's beautiful girlfriend",
-					"http://img5.imgtn.bdimg.com/it/u=900789529,877641406&fm=11&gp=0.jpg",
+					"http://www.1fanyi.com/data/attachment/forum/201608/17/230211ko7gpttpe6eqgces.jpg",
 					"https://www.youtube.com/watch?v=Xs0AupRWyC0")					
 MaGirl_5 = media.Movie("MaLong", "Ma long's beautiful girlfriend",
-					"http://img5.imgtn.bdimg.com/it/u=900789529,877641406&fm=11&gp=0.jpg",
+					"http://picture.youth.cn/xwjx/201608/W020160812353741177087.jpg",
 					"https://www.youtube.com/watch?v=Xs0AupRWyC0")
 # print(funnyVedio.discription)
 # print (MaGirl.discription)
